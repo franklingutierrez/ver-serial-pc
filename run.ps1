8bd87f774f9a222533d6bd46712ed5b240d69d2b
Franklin Gutierrez <47339569+franklingutierrez@users.noreply.github.com>
2026-02-16 14:30:41 -0500

    Add PowerShell script to download and run BAT file

---
+++ run.ps1
@@ -0,0 +1,11 @@
+# URL del archivo BAT en GitHub RAW
+$batUrl = "https://raw.githubusercontent.com/franklingutierrez/ver-serial-pc/main/Ver_serial.bat"
+
+# Ruta temporal donde se guardará el BAT
+$tempBat = "$env:TEMP\Ver_serial.bat"
+
+# Descargar el BAT
+Invoke-WebRequest -Uri $batUrl -OutFile $tempBat -UseBasicParsing
+
+# Ejecutar el BAT
+Start-Process -FilePath $tempBat -Wait
